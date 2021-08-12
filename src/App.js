89f6383329dba6226f1d1e9d3f331d135ea02990
eunjin0212 <eunjin9639@gmail.com>
2021-08-13 06:21:51 +0900

    gh-pages

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
   return (
     <HashRouter>
       <Navigation />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/movie/:id" component={Detail} />
+      <Route path='./' exact component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/movie/:id' component={Detail} />
     </HashRouter>
   );
 }
